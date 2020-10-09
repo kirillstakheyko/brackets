a1d899696637d1f7ef28ece3f3193fac2b955a5e
kirill.stakheyko <kirill.stakheyko@gmail.com>
2020-10-09 15:40:43 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  let isDeleted =true;
+while(str.length > 0 && isDeleted){
+    isDeleted = false;  
+  
+    for (let i = 0; i < bracketsConfig.length; i++){
+      for(let strItem = 0; strItem < str.length; strItem++){
+          if((str[strItem] === bracketsConfig[i][0]) && 
+          (str[strItem + 1] === bracketsConfig[i][1])){
+            str.splice(strItem, 2);
+            isDeleted = true;
+          }
+      }
+  }
+}
+return str.length > 0 ? false : true;
 }
